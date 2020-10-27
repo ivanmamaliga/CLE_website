09203ee73da7f7518c2c314f0776720e4bba6c9a
ivanmamaliga <70662242+ivanmamaliga@users.noreply.github.com>
2020-10-27 17:37:28 -0400

    Added content to form.js

--- js/form.js
+++ js/form.js
@@ -1,1 +1,36 @@
-console.log("HelloJS!");
+// User puts-in the numbers for the investments. 
+//Get the number from the input field
+
+var inv = document.getElementById('inv');
+// console.log(inv);
+// form.style.backgroundColor = "orange";
+// inv.style.backgroundColor = "white";
+// inv.style.color = "black"
+
+// inv.value = 2;
+
+//user puts in the numbers for the Savings
+
+var svn = document.getElementById('sav');
+// svn.style.backgroundColor = "blue";
+// console.log("Hello");
+//user clicks on the Submit btn and the script calculates the Payback period
+//let payoff = document.getElementById('poff');
+// payoff.value = 20;
+let button = document.getElementById('btn-submit');
+// button.style.color = "green";
+
+/////// The payback number is populated in the Payback cell
+
+button.onclick = () => {
+    
+    document.getElementById('poff').value = parseFloat(inv.value) + parseFloat(svn.value);
+    //document.write(payoff.value); // - writes in a new page
+    // alert("Hello");
+    // console.log("clicked");
+};
+  
+
+
+
+//User clicks on the Reset button and the fields a reset to null
