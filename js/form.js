3e147caf920a7f9c17a4bb4e8d9c6187af3e00f6
ivanmamaliga <70662242+ivanmamaliga@users.noreply.github.com>
2020-11-10 11:13:12 -0500

    Cleaned js file

--- js/form.js
+++ js/form.js
@@ -2,25 +2,16 @@
 //Get the number from the input field
 
 var inv = document.getElementById('inv');
-// console.log(inv);
-// form.style.backgroundColor = "orange";
-// inv.style.backgroundColor = "white";
-// inv.style.color = "black"
 
-// inv.value = 2;
 
 //user puts in the numbers for the Savings
 
 var svn = document.getElementById('sav');
-// svn.style.backgroundColor = "blue";
-// console.log("Hello");
-//user clicks on the Submit btn and the script calculates the Payback period
-//let payoff = document.getElementById('poff');
-// payoff.value = 20;
+
 var int = document.getElementById('int');
 
 let button = document.getElementById('btn-submit');
-// button.style.color = "green";
+
 
 /////// The payback number is populated in the Payback cell
 
