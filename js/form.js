b7a7f3beb9e77aae0f5b2c1ead90c578c9e44b8f
ivanmamaliga <ivanmamaliga@gmail.com>
2020-11-15 13:40:10 -0500

    added form verification

--- js/form.js
+++ js/form.js
@@ -1,34 +1,28 @@
-// User puts-in the numbers for the investments. 
-//Get the number from the input field
+
 
 var inv = document.getElementById('inv');
 
 
-//user puts in the numbers for the Savings
+
 
 var svn = document.getElementById('sav');
 
+
+
 var int = document.getElementById('int');
 
-let button = document.getElementById('btn-submit');
 
-
-/////// The payback number is populated in the Payback cell
-
-// button.onclick = () => {
-    
-//     document.getElementById('poff').value = parseFloat(inv.value) + parseFloat(svn.value);
-//     //document.write(payoff.value); // - writes in a new page
-//     // alert("Hello");
-//     // console.log("clicked");
-// };
   
 document.getElementById('calc').addEventListener('submit', function(e) {
     e.preventDefault(); //to prevent form submission
     // alert('click');
-    document.getElementById('poff').value = parseFloat(inv.value) * (parseFloat(int.value)/100 + 1) / parseFloat(svn.value);
-    }); 
-
+   
+    if (inv.value.length == 0 || svn.value.length == 0 || int.value.length == 0) {
+      alert("PLease enter the values an all required cells");
+    } else {
+        document.getElementById('poff').value = (parseFloat(inv.value) * (parseFloat(int.value)/100 + 1) / parseFloat(svn.value)).toFixed(2);
+    }
+  });
 
 //User clicks on the Reset button and the fields a reset to null
 
