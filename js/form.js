22b1dec9bac296800f272dc5924d853fdce778ec
ivanmamaliga <ivanmamaliga@gmail.com>
2020-11-15 13:55:07 -0500

    appended years to payoff result

--- js/form.js
+++ js/form.js
@@ -20,7 +20,7 @@ document.getElementById('calc').addEventListener('submit', function(e) {
     if (inv.value.length == 0 || svn.value.length == 0 || int.value.length == 0) {
       alert("PLease enter the values an all required cells");
     } else {
-        document.getElementById('poff').value = (parseFloat(inv.value) * (parseFloat(int.value)/100 + 1) / parseFloat(svn.value)).toFixed(2);
+        document.getElementById('poff').value = (parseFloat(inv.value) * (parseFloat(int.value)/100 + 1) / parseFloat(svn.value)).toFixed(2) + " years";
     }
   });
 
